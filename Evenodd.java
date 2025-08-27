f862ef4c29ee2cd152cfeceba05e2b6a09bd7334
vindhya216 <vindhyashankar21@gmail.com>
2025-08-27 18:10:32 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,15 @@
+public class Evenodd
+{
+    public static void main(String[] args)
+    {
+        int num = 30;
+        if (num % 2 == 0)
+        {
+            System.out.println(num + " is even");
+        }
+        else
+        {
+            System.out.println(num + " is odd");
+        }
+    }
+}
