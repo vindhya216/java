332143c3dd59e74482836b877b484cde2a59b883
vindhya216 <vindhyashankar21@gmail.com>
2025-08-27 17:59:15 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Add
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("addition is " + sum);
+    }
+}
