fcf3b263c43ed9a58c2e48c998941f7a9abd3a25
vindhya216 <vindhyashankar21@gmail.com>
2025-09-16 15:11:30 +0530

    Add files via upload

---
+++ PrefixExample.java
@@ -0,0 +1,9 @@
+public class PrefixExample {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int result=++a*b+20;
+        System.out.println("result="+result);
+       
+    }
+}
