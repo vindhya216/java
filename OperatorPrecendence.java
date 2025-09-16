ad7a033c9a91976533a2499afda4da93c3c39969
vindhya216 <vindhyashankar21@gmail.com>
2025-09-16 15:04:44 +0530

    Add files via upload

---
+++ OperatorPrecendence.java
@@ -0,0 +1,9 @@
+public class OperatorPrecendence {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        int result=a+b*c/100;
+        System.out.println("result="+result);
+    }
+}
