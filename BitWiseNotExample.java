0f870bca696999f918c3f191a241654640e02ef0
vindhya216 <vindhyashankar21@gmail.com>
2025-09-16 15:13:46 +0530

    Add files via upload

---
+++ BitWiseNotExample.java
@@ -0,0 +1,7 @@
+public class BitWiseNotExample {
+    public static void main(String[] args) {
+        int a=10;
+        int result=(~a+1);
+        System.out.println("result="+result);
+    }
+}
