7503042c38b626d6e908cb47d0cb89bb345d4bd5
vindhya216 <vindhyashankar21@gmail.com>
2025-09-23 15:01:20 +0530

    Add files via upload

---
+++ ForEachExample.java
@@ -0,0 +1,9 @@
+public class ForEachExample {
+    public static void main(String[] args) {
+        // Using a for-each loop to print elements of an array
+        string[] fruits = {"Apple", "Banana", "Cherry", "Date", "Elderberry"};
+        for (String fruit : fruits) {
+            System.out.println("Fruit: " + fruit);
+        }
+    }
+}
