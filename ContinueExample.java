b6f785accd2fb5dd11efe7b940a94eda4ba9cd15
vindhya216 <vindhyashankar21@gmail.com>
2025-09-23 15:23:51 +0530

    Add files via upload

---
+++ ContinueExample.java
@@ -0,0 +1,14 @@
+public class ContinueExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                System.out.println("Skipping iteration " + i);
+                
+                continue; 
+            
+            }
+        
+        System.out.println("i=" + i);
+    }
+}
+}
